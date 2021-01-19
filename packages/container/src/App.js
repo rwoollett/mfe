6a6e6957885d7a0e51131f965c939b300d9589cf
Rodney Woollett <rodney.woollett@gmail.com>
2021-01-19 13:09:12 +1300

    updated text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 
 export default () => {
     return <div>
-        <h2>Hey there!</h2>
+        <h2>Hey there!!!!</h2>
         <hr/>
         <MarketingApp/>
     </div>;
