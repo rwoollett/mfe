bb6cd69320defc8c20ace62860fd64d289c660da
Rodney Woollett <rodney.woollett@gmail.com>
2021-01-21 11:40:11 +1300

    Navigation communication sync

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Landing from './components/Landing';
 import Pricing from "./components/Pricing";
@@ -8,15 +8,15 @@ const generateClassName = createGenerateClassName({
     productionPrefix: 'ma'
 });
 
-export default () => {
+export default ({history}) => {
     return (<div>
         <StylesProvider generateClassName={generateClassName}>
-            <BrowserRouter>
+            <Router history={history}>
                 <Switch>
                     <Route exact path ="/pricing" component={Pricing}/>
                     <Route exact path="/" component={Landing}/>
                 </Switch>
-            </BrowserRouter>
+            </Router>
         </StylesProvider>
     </div>)
 };
